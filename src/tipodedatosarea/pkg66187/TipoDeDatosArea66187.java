e10c2cfe7b3dbb90448a610ede07320cadc7bc6b
DELL <DELL@DELL-PC>
2021-03-01 08:16:54 -0600

    comit

---
+++ src/tipodedatosarea/pkg66187/TipoDeDatosArea66187.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipodedatosarea.pkg66187;
+
+/**
+ *
+ * @author Alondra Palma
+ */
+public class TipoDeDatosArea66187 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    //Elabore un programa que utilice dos valores que representan 
+    //los lados de un rectángulo y encuentre el área del mismo.
+    
+        //Declarar variables
+        int base = 6;
+        int altura = 4;
+        
+        // ÁREA DEL RECTANGULO ES BASE X ALTURA   
+        int area_rectan = base * altura;
+        
+        System.out.println("El área del rectangulo es: " + area_rectan);
+    }
+    
+}
